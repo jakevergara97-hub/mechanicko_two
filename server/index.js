78e509ff29e1e9a1ca261003a35e15c300ec4240
Jake Vergara <jakevergara97@gmail.com>
2026-06-29 13:09:11 +0800

    Feature: Can now render the static location.

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+// const PORT = 3000;
+
+// Allows Express to read JSON from fetch requests
+app.use(express.json());
+app.use(cors()); // allow all origins (for development)
+
+app.get("/api/reverse-geocode/:lat/:lon", async (req, res) => {
+    console.log("Route hit!");
+    const { lat, lon } = req.params;
+
+    try {
+        const response = await fetch(
+            `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`,
+        {
+            headers: {
+                "User-Agent": "MechanicKo/1.0"
+                }
+            }
+        );
+
+        if(!response.ok) {
+            throw new Error(`Failed to get the address.`);
+        }
+
+        console.log(response.status);
+        const data = await response.json();
+        console.log(data);
+        res.json(data);
+    }
+    catch(error) {
+        res.status(500).json({
+            error: error.message
+        });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running...");
+});
---
+++ src/services/userLocation.js
@@ -0,0 +1,18 @@
+export async function getUserLocation() {
+    const lat = 13.759967;
+    const lon = 121.072737;
+
+    try {
+        const response = await fetch(`http://localhost:3000/api/reverse-geocode/${lat}/${lon}`);
+
+        if(!response.ok) {
+            throw new Error(`Failed to fetch address.`);
+        }
+
+        const data = await response.json();
+        return data;
+    }
+    catch(error) {
+        throw error;
+    }
+}
